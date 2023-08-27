ab18e2691601ff49e120416ed6930d8e27694c31
SahilMahadwar <mahadwarsahil@gmail.com>
2023-08-27 22:50:12 +0530

    feat: add getAllProducts controller

--- apps/api/src/controllers/products.ts
+++ apps/api/src/controllers/products.ts
@@ -1,9 +1,35 @@
 import { Request, Response } from "express";
+import { ProductModel } from "../models/Product";
 
 // @desc    Get All Products
 // @route   POST /api/v1/products
 // @access  Public
 // @role    User
 export const getAllProducts = async (req: Request, res: Response) => {
-  res.status(200).json({ success: true, data: { products: "product 1" } });
+  try {
+    let query;
+
+    // Copy req.query
+    const reqQuery = { ...req.query };
+
+    if (reqQuery.section_name) {
+      query = ProductModel.find({ section_name: req.query.section_name });
+    } else {
+      query = ProductModel.find();
+    }
+
+    const products = await query;
+
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      data: products.reverse(),
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error:
+        error instanceof Error ? error.message : "Request failed try again",
+    });
+  }
 };
