84864bf4d8d4d06365a02987988f8602af02228b
SahilMahadwar <mahadwarsahil@gmail.com>
2023-08-27 23:40:56 +0530

    feat: setup swiper

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import "swiper/css";
+import { register } from "swiper/element/bundle";
 import { Router } from "./routes/routes";
 
 export const App = () => {
+  useEffect(() => {
+    if (document.readyState === "complete") {
+      register();
+    }
+    // register Swiper custom elements
+  }, []);
+
   return <Router />;
 };
 
