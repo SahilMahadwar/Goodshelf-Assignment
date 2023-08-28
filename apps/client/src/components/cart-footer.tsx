f50311228ed106dd72d814e66b6f29a4d18f4c55
SahilMahadwar <mahadwarsahil@gmail.com>
2023-08-28 07:15:52 +0530

    feat: add cart page footer

---
+++ apps/client/src/components/cart-footer.tsx
@@ -0,0 +1,20 @@
+import { toast } from "react-toastify";
+import useCart from "../hooks/use-cart";
+
+export const CartFooter = () => {
+  const { cartValue } = useCart();
+
+  return (
+    <div className="flex items-center justify-between border-t px-6 py-4">
+      <h3 className="text-xl text-slate-800">
+        Total Cart Value: <span className="font-medium">₹{cartValue}</span>
+      </h3>
+      <button
+        onClick={() => toast.info(`Dummy button doesn't do anything`)}
+        className="rounded-lg bg-rose-500 px-4 py-3 text-white"
+      >
+        Check Out
+      </button>
+    </div>
+  );
+};
