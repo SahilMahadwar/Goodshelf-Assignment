71556f49f1155e3e0d56d161399f2c2a96a4c3a9
SahilMahadwar <mahadwarsahil@gmail.com>
2023-08-28 09:51:24 +0530

    fix: handle mongodb connect promise

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 import { env } from "../utils/env";
 
 export const connectDB = async () => {
-  mongoose.set("strictQuery", false);
+  try {
+    mongoose.set("strictQuery", false);
 
-  const conn = await mongoose.connect(env.MONGODB_URL);
+    const conn = await mongoose.connect(env.MONGODB_URL);
 
-  console.log(`📡[Mongodb]: DB Connected host: ${conn.connection.host}`);
+    console.log(`📡[Mongodb]: DB Connected host: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
 };
